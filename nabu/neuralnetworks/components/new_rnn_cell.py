ae2cc1063263b02f4283f7df9346e68772899793
D073179 <abdelaziz.ben.othman@sap.com>
2018-10-09 10:42:31 +0200

    add LNBasicLSTMCELL

--- nabu/neuralnetworks/components/new_rnn_cell.py
+++ nabu/neuralnetworks/components/new_rnn_cell.py
@@ -261,6 +261,58 @@ class LnGRUCell(tf.contrib.rnn.RNNCell):
 	return new_h, new_h
 
 
+#### basic lstm with layer normalization 
+
+class LnBasicLSTMCell(tf.contrib.rnn.RNNCell):
+
+  def __init__(self, num_units, forget_bias=1.0,
+               state_is_tuple=True, activation=None, reuse=None):
+    super(BasicLSTMCell, self).__init__(_reuse=reuse)
+    if not state_is_tuple:
+      logging.warn("%s: Using a concatenated state is slower and will soon be "
+                   "deprecated.  Use state_is_tuple=True.", self)
+    self._num_units = num_units
+    self._forget_bias = forget_bias
+    self._state_is_tuple = state_is_tuple
+    self._activation = activation or math_ops.tanh
+
+  @property
+  def state_size(self):
+    return (LSTMStateTuple(self._num_units, self._num_units)
+            if self._state_is_tuple else 2 * self._num_units)
+
+  @property
+  def output_size(self):
+    return self._num_units
+
+  def call(self, inputs, state):
+    with tf.variable_scope('layer_normalization'):
+      gain_h = tf.get_variable('gain_h', shape=[4*self._num_units], initializer=tf.ones_initializer())
+      bias_h = tf.get_variable('bias_h', shape=[4*self._num_units], initializer=tf.zeros_initializer())
+      gain_c = tf.get_variable('gain_c', shape=[self._num_units], initializer=tf.ones_initializer())
+      bias_c = tf.get_variable('bias_c', shape=[self._num_units], initializer=tf.zeros_initializer())
+    sigmoid = math_ops.sigmoid
+    # Parameters of gates are concatenated into one multiply for efficiency.
+    if self._state_is_tuple:
+      c, h = state
+    else:
+      c, h = array_ops.split(value=state, num_or_size_splits=2, axis=1)
+
+    concat = ln(_linear([inputs, h], 4 * self._num_units, True), gain_h, bias_h)
+
+    # i = input_gate, j = new_input, f = forget_gate, o = output_gate
+    i, j, f, o = array_ops.split(value=concat, num_or_size_splits=4, axis=1)
+
+    new_c = (
+        c * sigmoid(f + self._forget_bias) + sigmoid(i) * self._activation(j))
+    new_h = self._activation(ln(new_c, gain_c, bias_c)) * sigmoid(o)
+
+    if self._state_is_tuple:
+      new_state = LSTMStateTuple(new_c, new_h)
+    else:
+      new_state = array_ops.concat([new_c, new_h], 1)
+    return new_h, new_state
+
 
 
 
